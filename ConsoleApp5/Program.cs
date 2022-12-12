8aa3cbe4d6c51b7b74679e6779c72657d7dbf29e
Pavel3737 <118966084+Pavel3737@users.noreply.github.com>
2022-12-12 14:10:20 +0300

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Vis_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[5];
+            // input 
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine($"Введите число номер {i + 1}:");
+                nums[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Ваши числа");
+            //output
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine($"{nums[i]} ");
+            }
+        }
+    }
+}
